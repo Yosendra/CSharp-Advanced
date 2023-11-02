d7cf627313d8f9181c485981ff9e055283cb64d7
Yosendra <yosendra.evri@gmail.com>
2023-11-03 04:35:29 +0800

    Threading - 2. Thread Class

---
+++ 1. Threading/2-Thread-Class/MainThread/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Thread mainThread = Thread.CurrentThread;
+        mainThread.Name = "Main Thread";
+
+        // Get the thread's priority level
+        ThreadPriority priority = mainThread.Priority;
+
+        Console.WriteLine(mainThread.Name);
+        
+        // Output : Normal
+        Console.WriteLine(priority);
+
+        // Output : True
+        Console.WriteLine(mainThread.IsAlive);
+
+        // Output : False
+        // Indicate that Main Thread will prevent console application from closing
+        // Console application has to wait until the completion of Main Thread
+        // That is the completion of Main Method's excecution
+        Console.WriteLine(mainThread.IsBackground);
+
+        ThreadState state = mainThread.ThreadState;
+        // Output : Running
+        Console.WriteLine(state);
+
+        Console.ReadKey();
+    }
+}
