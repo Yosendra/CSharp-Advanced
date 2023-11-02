64d8475aacd21e748093783451d213f1f9ffbf07
Yosendra <yosendra.evri@gmail.com>
2023-11-02 17:17:43 +0800

    Threading - 1. Main Thread

---
+++ 1. Threading/1-MainThread/MainThread/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Access current working thread which is Main Thread
+        Thread mainThread = Thread.CurrentThread;
+        
+        // Giving name to the thread
+        mainThread.Name = "Main Thread";
+
+        // Invoke the .ToString()
+        Console.WriteLine(mainThread);
+        
+        // Print the thread's name we have given
+        Console.WriteLine(mainThread.Name);
+
+        Method1();
+    }
+
+    static void Method1() 
+    {
+        Console.WriteLine("Method 1");
+    }
+}
