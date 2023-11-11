1d4e230cb4c8030dc15c9037ddc4c8038f6180df
Yosendra <yosendra.evri@gmail.com>
2023-11-11 16:57:33 +0800

    Threading - 32. Mutex

---
+++ 1. Threading/32-Mutex/CSVExample/Shared.cs
@@ -0,0 +1,18 @@
+﻿namespace CSVExample
+{
+    public class Shared
+    {
+        public static Mutex mutex { get; set; }
+        public static string FilePath { get; set; }
+        public static int ChunkSize { get; set; }
+        public static int MaxConcurrency { get; set; }
+
+        static Shared()
+        {
+            mutex = new Mutex();
+            FilePath = "data.csv";
+            ChunkSize = 100;
+            MaxConcurrency = 3;
+        }
+    }
+}
