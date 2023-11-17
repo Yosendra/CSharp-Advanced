8a43152f39e80073f1832d3d7c0a8054092c7330
Yosendra <yosendra.evri@gmail.com>
2023-11-18 00:02:17 +0800

    Asynchronous Programming - 5.1 Testing Exception Propagate

---
+++ 3. Asynchronous Programming/5.1-Testing-Exception-Propagate/ExceptionPropagate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+    public async static Task Main()
+    {
+        Program program = new();
+
+        try
+        {
+            await program.A();
+            Console.WriteLine("Finish executing Main"); // This line will not be executed because the exception thrown
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+        Console.ReadKey();
+    }
+
+    private async Task A()
+    {
+        await this.B();
+        Console.WriteLine("Finish executing A"); // This line will not be executed because the exception thrown
+    }
+
+    private async Task B()
+    {
+        await this.C();
+        Console.WriteLine("Finish executing B"); // This line will not be executed because the exception thrown
+    }
+
+    private async Task C()
+    {
+        throw new NotImplementedException();
+    }
+}
