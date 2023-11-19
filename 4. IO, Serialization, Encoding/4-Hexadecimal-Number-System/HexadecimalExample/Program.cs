810b51f1e47b52d9258d735d8ecdf4e1200a97f8
Yosendra <yosendra.evri@gmail.com>
2023-11-19 17:08:21 +0800

    IO - 4. Hexadecimal Number System

---
+++ 4. IO, Serialization, Encoding/4-Hexadecimal-Number-System/HexadecimalExample/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int _decimal = 742;
+        
+        string _hexadecimal = Convert.ToString(_decimal, 16);       // Decimal to Hexadecimal
+        Console.WriteLine(_hexadecimal);
+
+        _decimal = Convert.ToInt32(_hexadecimal, 16);               // Hexadecimal to Decimal
+        Console.WriteLine(_decimal);
+
+        // Hexadecimal literal
+        var hexadecimal_literal = 0x64;
+        Console.WriteLine(hexadecimal_literal);
+
+        Console.ReadKey();
+    }
+}
