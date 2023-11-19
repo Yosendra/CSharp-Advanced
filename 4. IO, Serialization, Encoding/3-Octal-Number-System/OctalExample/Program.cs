3c66fd36fb2621e042575375f5d622a7ae41757f
Yosendra <yosendra.evri@gmail.com>
2023-11-19 16:51:21 +0800

    IO - 3. Octal Number System

---
+++ 4. IO, Serialization, Encoding/3-Octal-Number-System/OctalExample/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int _decimal = 289;
+        
+        string _octal = Convert.ToString(_decimal, 8);      // Decimal to Octal
+        Console.WriteLine(_octal);
+
+        _decimal = Convert.ToInt32(_octal, 8);              // Octal to Decimal
+        Console.WriteLine(_decimal);
+
+        // There is no literal value for Octal number in C#
+
+        Console.ReadKey();
+    }
+}
