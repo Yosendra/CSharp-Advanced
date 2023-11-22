e6a12ec39b942d51dc50fe5d66d0225b97d4b240
Yosendra <yosendra.evri@gmail.com>
2023-11-22 22:32:28 +0800

    IO - 10. System.IO.File Class: Read & Write

---
+++ 4. IO, Serialization, Encoding/10-System.IO.File-Class-Read-&-Write/FileLineExample/FileLineExample/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> asia = new()
+        {
+            "Indonesia", "Singapore", "Malaysia",
+        };
+
+        // WriteAllLines
+        string filePath = @"c:\practice\asia.txt";
+        File.WriteAllLines(filePath, asia);
+        Console.WriteLine("asia.txt created");
+
+        // ReadAllLines
+        string[] asiaCountries = File.ReadAllLines(filePath);
+        foreach (var country in asiaCountries)
+        {
+            Console.WriteLine(country);
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ 4. IO, Serialization, Encoding/10-System.IO.File-Class-Read-&-Write/FileReadWriteExample/Program.cs
@@ -0,0 +1,21 @@
+﻿class MyClass
+{
+    public static void Main()
+    {
+        string filePath = @"c:\practice\dog.txt";
+        string content = "The dog is one of the common domestic animal.";
+
+        // WriteAllText
+        File.WriteAllText(filePath, content);
+        Console.WriteLine("dog.txt created");
+
+        // ReadAllText
+        // This will read all the text in the file to a single string
+        // even when there are new lines in it.
+        // You can see it in the debugger.
+        content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+
+        Console.ReadKey();
+    }
+}
