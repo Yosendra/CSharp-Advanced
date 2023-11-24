7f6ad0ca1acb1d94cc4a0440612317b02ff64b02
Yosendra <yosendra.evri@gmail.com>
2023-11-24 17:45:27 +0800

    IO - 12. System.IO.FileInfo Class: Properties

---
+++ 4. IO, Serialization, Encoding/12-System.IO.FileInfo-Class-Properties/FileInfoPropertiesExample/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string filePath = @"c:\practice\japan.txt";
+        FileInfo fileInfo = new(filePath);
+
+        if (fileInfo.Exists)
+        {
+            Console.WriteLine($"Exists? : {fileInfo.Exists}");
+            Console.WriteLine($"Fullname : {fileInfo.FullName}");
+            Console.WriteLine($"Name : {fileInfo.Name}");
+            Console.WriteLine($"DirectoryName : {fileInfo.DirectoryName}");
+            Console.WriteLine($"Extension : {fileInfo.Extension}");
+            Console.WriteLine($"LastWriteTime : {fileInfo.LastWriteTime}");
+            Console.WriteLine($"LastAccessTime : {fileInfo.LastAccessTime}");
+            Console.WriteLine($"CreationTime : {fileInfo.CreationTime}");
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+
+        Console.ReadKey();
+    }
+}
