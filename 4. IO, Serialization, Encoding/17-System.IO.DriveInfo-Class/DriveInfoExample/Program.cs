34d26b52cee9c827d0682a85ba39416f236a16cd
Yosendra <yosendra.evri@gmail.com>
2023-11-25 15:07:56 +0800

    IO - 17. System.IO.DriveInfo Class

---
+++ 4. IO, Serialization, Encoding/17-System.IO.DriveInfo-Class/DriveInfoExample/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        DriveInfo driveInfo = new("c:");
+        Console.WriteLine($"Name : {driveInfo.Name}");
+        Console.WriteLine($"DriveType : {driveInfo.DriveType}");
+        Console.WriteLine($"VolumeLabel : {driveInfo.VolumeLabel}");
+        Console.WriteLine($"RootDirectory : {driveInfo.RootDirectory}");
+        Console.WriteLine($"TotalSize : {driveInfo.TotalSize / 1024 / 1024 / 1024} GB");
+        Console.WriteLine($"AvailableFreeSpace : {driveInfo.AvailableFreeSpace / 1024 / 1024 / 1024} GB");
+
+        Console.ReadKey();
+    }
+}
