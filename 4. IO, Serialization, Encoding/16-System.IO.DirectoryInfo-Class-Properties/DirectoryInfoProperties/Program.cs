e3a573bd1b643de18d8bd7e15b1e4f6656e18eaa
Yosendra <yosendra.evri@gmail.com>
2023-11-25 14:55:33 +0800

    IO - 16. System.IO.DirectoryInfo Class: Properties

---
+++ 4. IO, Serialization, Encoding/16-System.IO.DirectoryInfo-Class-Properties/DirectoryInfoProperties/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string dirPath = @"c:\practice\world";
+        DirectoryInfo directoryInfo = new(dirPath);
+
+        Console.WriteLine($"Exists? : {directoryInfo.Exists}");
+
+        if (directoryInfo.Exists)
+        {
+            Console.WriteLine($"Fullname : {directoryInfo.FullName}");
+            Console.WriteLine($"Name : {directoryInfo.Name}");
+            Console.WriteLine($"Parent : {directoryInfo.Parent}");
+            Console.WriteLine($"Root : {directoryInfo.Root}");
+            Console.WriteLine($"CreationTime : {directoryInfo.CreationTime}");
+            Console.WriteLine($"LastAccessTime : {directoryInfo.LastAccessTime}");
+            Console.WriteLine($"LastWriteTime : {directoryInfo.LastWriteTime}");
+        }
+        else
+        {
+            Console.WriteLine("Folder not found");
+        }
+
+        Console.ReadKey();
+    }
+}
