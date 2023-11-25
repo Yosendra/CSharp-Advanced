e93520f9761a1e16a7dce72cafe8f8710f5a34d8
Yosendra <yosendra.evri@gmail.com>
2023-11-25 16:29:15 +0800

    IO - 18. System.IO.FileStream Class Part 1

---
+++ 4. IO, Serialization, Encoding/18-System.IO.FileStream-Class-Part-1/FileStreamExample/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string filePath = @"c:\practice\dog.txt";
+        FileInfo fileInfo = new(filePath);
+
+        // There many ways to create a file in C#, using FileStream, File, FileInfo class
+        // The point to note here is the methods that are invoked returns FileStream object
+        //FileStream fileStream = new(filePath, FileMode.Create, FileAccess.Write);
+        //FileStream fileStream = File.Create(filePath);
+        //FileStream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write);
+        //FileStream fileStream = File.OpenWrite(filePath);
+        //FileStream fileStream = fileInfo.Create();
+        //FileStream fileStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
+        FileStream fileStream = fileInfo.OpenWrite();
+
+        Console.ReadKey();
+    }
+}
