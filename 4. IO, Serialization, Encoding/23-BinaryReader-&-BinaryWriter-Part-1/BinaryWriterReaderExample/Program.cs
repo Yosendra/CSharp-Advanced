eadfb6d18e58e9284f9b8b5a0ec013f3ca866d22
Yosendra <yosendra.evri@gmail.com>
2023-11-26 17:18:12 +0800

    IO - 23. BinaryReader & BinaryWriter Part 1

---
+++ 4. IO, Serialization, Encoding/23-BinaryReader-&-BinaryWriter-Part-1/BinaryWriterReaderExample/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        short countryId = 1;
+        string countryName = "France";
+        long population = 65273511;
+        string region = "Western Europe";
+        string filePath = @"c:\practice\france.txt";
+        
+        // BinaryWriter write binary data format to a file
+        FileStream fileStream = new(filePath, FileMode.Create, FileAccess.Write);
+        using (BinaryWriter binaryWriter = new(fileStream))
+        {
+            binaryWriter.Write(countryId);
+            binaryWriter.Write(countryName);
+            binaryWriter.Write(population);
+            binaryWriter.Write(region);
+        }
+        Console.WriteLine("france.txt created");
+
+        Console.ReadKey();
+    }
+}
