210f8cda57b6207af6a53170b575ac2e0985d5ee
Yosendra <yosendra.evri@gmail.com>
2023-11-27 19:46:33 +0800

    .NET 6 Features - 1. Top Level Statement

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/1. Top Level Statement/TopLevelStatement/Program.cs
@@ -0,0 +1,22 @@
+﻿// There is no need to encapsulate the statements inside Main() function anymore
+
+// Warning
+// 1. We cannot define class or namespace above the statement
+// 2. Only one file can have top-level statement in one project
+// 3. Cannot use local variable or local function declared in a top-level statement
+// 4. Top-level statements automatically will be encapsulated in Main() function of Program class at compilation time
+
+string user = "Yosi";
+Console.WriteLine("Hello, World! " + user);
+Console.ReadKey();
+
+namespace Namespace1
+{
+    class MyClass
+    {
+        // cannot access local variable in top-level statement
+        // string name = user;
+    }
+}
+
+//class Program {}  // This will be error too if we use top-level statement
