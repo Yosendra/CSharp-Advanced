f8319bc27b94d28f1faf30887505c12ccbffad70
Yosendra <yosendra.evri@gmail.com>
2023-11-28 22:46:27 +0800

    .NET 6 Features - 16. Pattern Matching: Positional Pattern

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/16. Pattern Matching - Positional Pattern/PatternMatchingExample/Program.cs
@@ -0,0 +1,152 @@
+﻿enum MaritalStatus
+{
+    Unmarried, Married
+}
+
+class Person
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+    public string? Gender { get; set; }
+    public MaritalStatus PersonMaritalStatus { get; set; }
+
+    public void Deconstruct(out Person person, out string? gender, out int? age, out MaritalStatus maritalStatus)
+    {
+        // Can be writen traditionaly like this
+        //person = this;
+        //gender = this.Gender;
+        //age = this.Age;
+        //maritalStatus = this.PersonMaritalStatus;
+
+        // or more fancy way like this using tupple
+        (person, gender, age, maritalStatus) = (this, this.Gender, this.Age, this.PersonMaritalStatus);
+    }
+}
+
+class Employee : Person
+{
+    public double? Salary { get; set; }
+}
+
+class Customer : Person
+{
+    public double? CustomerBalance { get; set; }
+}
+
+class Supplier : Person
+{
+    public double? SupplierBalance { get; set; }
+}
+
+class Manager : Employee
+{
+    
+}
+
+class Descripter
+{
+    public static string GetDescription(Person person)
+    {
+        // 'switch-case pattern' here used to check person type also cast it to another type
+        switch (person)
+        {
+            case Employee employee:
+                return $"{person.Name}, {person.Age}, {person.Gender}, {employee.Salary}";
+
+            case Customer customer:
+                return $"{person.Name}, {person.Age}, {person.Gender}, {customer.CustomerBalance}";
+
+            case Supplier supplier:
+                return $"{person.Name}, {person.Age}, {person.Gender}, {supplier.SupplierBalance}";
+
+            default:
+                return $"{person.Name}, {person.Age}, {person.Gender}";
+        }
+    }
+
+    public static string GetDescriptionAge(Person person)
+    {
+        // Property pattern
+
+        string result = person switch
+        {
+            Person { Age: < 20 and >= 13 }
+                p => $"{p.Name} is a teenager",
+
+            Person { Age: < 13 }
+                p => $"{p.Name} is a child",
+
+            Person { Age: >= 20 and < 60 }
+                p => $"{p.Name} is an adult",
+
+            Person { Age: >= 60 and not (100 or 200) }
+                p => $"{p.Name} is a senior citizen",
+
+            Person { Age: 100 or 200 }
+                p => $"{p.Name} is a centenarian",
+
+            _
+                => $"{person.Name} is a person"
+        };
+
+        return result;
+    }
+
+    public static string GetDescriptionMaritalStatus(Person person)
+    {
+        // Positional Pattern
+
+        // Deconstruct will be invoked automatically by switch statement and return tuple equal like below
+        // (person, person.Gender, person.Age, person.PersonMaritalStatus)
+        // We just only use 'person' object in the switch
+        return person switch
+        {
+            (Person, "Female", _, MaritalStatus.Unmarried) 
+                => $"Miss. {person.Name}",
+
+            (Person, "Female", _, MaritalStatus.Married)
+                => $"Mrs. {person.Name}",
+
+            (Person, "Male", < 18, _)
+                => $"Master {person.Name}",
+
+            (Person, "Male", >= 18, _)
+                => $"Mr. {person.Name}",
+
+            (Person, not("Female" or "Male"), _, _)
+                => $"Mx. {person.Name}",
+
+            _ => $"{person.Name}"
+        };
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Manager manager = new()
+        {
+            Name = "John",
+            Gender = "Male",
+            Age = 20,
+            Salary = 3000,
+            PersonMaritalStatus = MaritalStatus.Married,
+        };
+
+        Customer customer = new()
+        {
+            Name = "Smith",
+            Gender = "Male",
+            Age = 100,
+            CustomerBalance = 1000,
+            PersonMaritalStatus = MaritalStatus.Unmarried,
+        };
+
+        Console.WriteLine(Descripter.GetDescription(customer));
+        Console.WriteLine(Descripter.GetDescriptionAge(customer));
+        Console.WriteLine(Descripter.GetDescriptionMaritalStatus(customer));
+
+        Console.ReadKey();
+    }
+}
