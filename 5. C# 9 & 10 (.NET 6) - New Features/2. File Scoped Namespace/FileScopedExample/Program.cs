7e44b2b54bca2effceada30bb2eb3a7b4d81c361
Yosendra <yosendra.evri@gmail.com>
2023-11-28 12:04:10 +0800

    .NET 6 Features - 2. File Scoped Namespace

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/2. File Scoped Namespace/FileScopedExample/Program.cs
@@ -0,0 +1,20 @@
+﻿// College - Student, Teacher
+
+// Traditional Way, namespace use curly braced
+//namespace College
+//{
+//    class Student {}
+//    class Teacher {}
+//}
+
+// But in file scoped namespace, only one namespace is allowed
+
+// - namespace statement can be declared before or after using statement
+using System;
+namespace College;
+using System.IO;
+
+class Student { }
+class Teacher { }
+
+// Using semicolon at the end of namespace statement, both class still in College namespace
---
+++ 5. C# 9 & 10 (.NET 6) - New Features/2. File Scoped Namespace/FileScopedExample/Something.cs
@@ -0,0 +1,13 @@
+﻿// We decide top-level statment here
+Something something = new();
+Console.WriteLine(something.student);
+Console.WriteLine(something.teacher);
+Console.ReadKey();
+
+// This class will not be included in College namespace, and do not belong to any namespace
+class Something
+{
+    // We can access College namespace here
+    public College.Student student = new College.Student();
+    public College.Teacher teacher = new College.Teacher();
+}
