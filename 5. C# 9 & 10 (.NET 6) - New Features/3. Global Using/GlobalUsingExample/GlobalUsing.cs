13455f779dedd04768532b388a18efbbe3dedaf3
Yosendra <yosendra.evri@gmail.com>
2023-11-28 12:22:16 +0800

    .NET 6 Features - 3. Global Using

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/3. Global Using/GlobalUsingExample/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿// Best practice to use seperate file for using 'global using' statement
+// These import in this file will be also imported in every file in entire project
+
+global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Collections.Generic;
---
+++ 5. C# 9 & 10 (.NET 6) - New Features/3. Global Using/GlobalUsingExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// We need not to import Generic namespace to use List<> collection
+// also we can use linq immediately without importing System.Linq namespace in this file
+
+List<int> numbers = new()
+{
+    1, 2, 3
+};
+
+var backUpNumbers = numbers.Select(x => x).ToList();
+foreach (var number in backUpNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.ReadKey();
