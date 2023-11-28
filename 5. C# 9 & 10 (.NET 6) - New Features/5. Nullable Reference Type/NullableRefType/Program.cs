9fd77fa0598be13cdf8c464c95a08f7869184632
Yosendra <yosendra.evri@gmail.com>
2023-11-28 13:59:21 +0800

    .NET 6 - 5. Nullable Reference Type

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/5. Nullable Reference Type/NullableRefType/Program.cs
@@ -0,0 +1,32 @@
+﻿class Employee { public int x = 19; }
+
+// Employee  => Non-nullable reference type (null value is not accepted)
+// Employee? => Nullable reference type (null value is accepted)
+// By default all classes and interfaces are 'non-nullable reference type' to convert them as 'nullable reference type', suffix a question mark (?)
+
+class EmployeeBusinessLogic
+{
+    public Employee? GetEmployee()
+    {
+        return null;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+
+        Employee? employee = employeeBusinessLogic.GetEmployee();
+        if (employee != null)
+        {
+            Console.WriteLine(employee.x);
+        }
+        else
+        {
+            Console.WriteLine("employee is null");
+        }
+        Console.ReadKey();
+    }
+}
