29f1192a4857c711871754dc18ccd2e4a3d97e2b
Yosendra <yosendra.evri@gmail.com>
2023-11-29 22:26:49 +0800

    .NET 6 Features - 25. Record: Immutability

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/25. Record - Immutability/RecordExample/Program.cs
@@ -0,0 +1,26 @@
+﻿public record Address(string City);
+
+// We can make property mutable in record in this way
+public record Person(string Name, int Age, Address PersonAddress)
+{
+    // here are mutable property in record
+    public int Property1 { get; set; }
+};
+
+class Program
+{
+    static void Main()
+    {
+        // When object initialization
+        Person person1 = new("John", 20, new("London"));
+        Person person2 = new Person("Sylvia", 30, new Address("Manhattan"));
+
+        // We can assign to a record's property
+        person1.Property1 = 10;
+
+        Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}");
+        Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}");
+
+        Console.ReadKey();
+    }
+}
