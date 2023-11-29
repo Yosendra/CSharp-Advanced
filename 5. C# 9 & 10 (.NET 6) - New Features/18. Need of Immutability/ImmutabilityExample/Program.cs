67ddfdd57e8da4608edfe2e2232a313fbb4705e7
Yosendra <yosendra.evri@gmail.com>
2023-11-29 16:25:34 +0800

    .NET 6 Features - 18. Need of Immutability

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/18. Need of Immutability/ImmutabilityExample/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+List<BankAccount> bankAccounts = DataStorage.GetBankAccounts();
+BankAccount firstBankAccount = bankAccounts[0];
+
+double balance = DataStorage.GetCurrentBalance(firstBankAccount);
+
+Console.WriteLine($"AccountNumber: {firstBankAccount.AccountNumber}, balance: {balance}");
+Console.ReadKey();
+
+class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public double CurrentBalance { get; set; }
+}
+
+class DataStorage
+{
+    // Done by You (Developer 1)
+    public static List<BankAccount> GetBankAccounts()
+    {
+        return new()
+        {
+            new(){ AccountNumber = 1, CurrentBalance = 1000 },
+            new(){ AccountNumber = 2, CurrentBalance = 2000 },
+        };
+    }
+
+    // Done by Other (Developer 2)
+    public static double GetCurrentBalance(BankAccount bankAccount)
+    {
+        // Unexpectedly they change the value of Account Number.
+        // Need immutability for AccountNumber to keep data integrity
+        bankAccount.AccountNumber = 1000;
+        return bankAccount.CurrentBalance;
+    }
+}
+
