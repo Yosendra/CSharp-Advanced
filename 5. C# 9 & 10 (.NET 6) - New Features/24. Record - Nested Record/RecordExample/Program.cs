2000848f8165b7433c253529a21a5c96df350517
Yosendra <yosendra.evri@gmail.com>
2023-11-29 21:06:49 +0800

    .NET 6 Features - 24. Record: Nested Record

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/24. Record - Nested Record/RecordExample/Program.cs
@@ -0,0 +1,18 @@
+﻿public record Address(string City);
+// We set Address record as the property, so it nested in Person record
+public record Person(string Name, int Age, Address PersonAddress);
+
+class Program
+{
+    static void Main()
+    {
+        // When object initialization
+        Person person1 = new("John", 20, new("London"));
+        Person person2 = new Person("Sylvia", 30, new Address("Manhattan"));
+
+        Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}");
+        Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}");
+
+        Console.ReadKey();
+    }
+}
