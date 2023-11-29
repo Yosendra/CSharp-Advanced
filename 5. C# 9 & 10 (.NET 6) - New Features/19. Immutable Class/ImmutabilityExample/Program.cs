fc8bece0571f69d1771dbe450b744f800bbb4f49
Yosendra <yosendra.evri@gmail.com>
2023-11-29 16:45:13 +0800

    .NET 6 Features - 19. Immutable Class

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/19. Immutable Class/ImmutabilityExample/Program.cs
@@ -0,0 +1,66 @@
+﻿// Immutability Class => class that contain read-only fields and read-only properties.
+
+List<BankAccount> bankAccounts = DataStorage.GetBankAccounts();
+BankAccount firstBankAccount = bankAccounts[0];
+
+double balance = DataStorage.GetCurrentBalance(firstBankAccount);
+
+Console.WriteLine($"AccountNumber: {firstBankAccount.AccountNumber}, balance: {balance}");
+Console.ReadKey();
+
+class BankAccount
+{
+    // Use readonly modifier in the fields, can only be assigned a value on constructor
+    #region Fields
+
+    private readonly int _accountNumber;
+    private readonly double _currentBalance;
+
+    #endregion of Fields
+
+    // Set prperties to be getter only
+    #region Properties
+    public int AccountNumber 
+    {
+        get => _accountNumber;
+    }
+
+    public double CurrentBalance
+    {
+        get => _currentBalance;
+    }
+
+    #endregion of Properties
+
+    // Constructor
+    public BankAccount(int accountNumber, double currentBalanca)
+    {
+        _accountNumber = accountNumber;
+        _currentBalance = currentBalanca;
+    }
+}
+
+class DataStorage
+{
+    // Done by You (Developer 1)
+    public static List<BankAccount> GetBankAccounts()
+    {
+        return new()
+        {
+            new(1, 1000),
+            new(2, 2000),
+        };
+    }
+
+    // Done by Other (Developer 2)
+    public static double GetCurrentBalance(BankAccount bankAccount)
+    {
+        // Unexpectedly they change the value of Account Number.
+        // Need immutability for AccountNumber to keep data integrity
+        
+        // After implementing readonly property, this assignment become an error to compiler
+        //bankAccount.AccountNumber = 1000;
+        return bankAccount.CurrentBalance;
+    }
+}
+
