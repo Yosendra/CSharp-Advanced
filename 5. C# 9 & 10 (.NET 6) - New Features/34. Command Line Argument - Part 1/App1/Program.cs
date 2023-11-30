0a9888b5cac36511c49244d54673f87b6faa0a6a
Yosendra <yosendra.evri@gmail.com>
2023-11-30 22:22:05 +0800

    .NET 6 Features - 34. Command Line Argument - Part 1

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/34. Command Line Argument - Part 1/App1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Run this program through Command Prompt. 
+// Ex: app1 1 2 3 4 5
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{args.Length} values found.");
+        int sum = 0;
+        foreach (var arg in args)
+        {
+            if (int.TryParse(arg, out int convertedValue))
+            {
+                sum += convertedValue;
+            }
+            Console.WriteLine($"out int: {convertedValue}");
+        }
+        Console.WriteLine($"Total of numbers: {sum}");
+
+        Console.ReadKey();
+    }
+}
