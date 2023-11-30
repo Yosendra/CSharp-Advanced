ef58d37a0554b7f270dfbef8535a0ac26dfdd6dd
Yosendra <yosendra.evri@gmail.com>
2023-11-30 14:07:01 +0800

    .NET 6 Features - 29. Record: ToString()

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/29. Record - ToString()/RecordExample/Program.cs
@@ -0,0 +1,21 @@
+﻿public record Person(string Name, int Age, Address PersonAddress);
+public record Address(string City, string Country)
+{
+    public override string ToString()   // We are overriding the '.ToString()' method
+    {
+        return $"City is {City} in {Country}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new("John", 20, new("London", "UK"));
+        
+        Console.WriteLine($"{person}");
+        Console.WriteLine($"{person.PersonAddress}");
+        
+        Console.ReadKey();
+    }
+}
