6b15d29b0db3b601fcf0008b004cd258d1d78604
Yosendra <yosendra.evri@gmail.com>
2023-11-30 18:34:26 +0800

    .NET 6 Features - 32. Record: 'sealed' .ToString()

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/32. Record - 'sealed' .ToString()/RecordExample/Program.cs
@@ -0,0 +1,44 @@
+﻿public record Person(string? Name, 
+    DateTime? DateOfBirth, 
+    int? Age)
+{
+    public Person(string? name, DateTime? dateOfBirth) : this(name, dateOfBirth, null)
+    {
+        if (dateOfBirth is not null)
+        {
+            Age = Convert.ToInt32(DateTime.Now.Subtract(dateOfBirth.Value).TotalDays / 365.25);
+        }
+    }
+
+    public Person() : this(null, null, null){}
+
+    public string GetName()
+    {
+        return $"Ms./Mr. {Name}";
+    }
+
+    public override string ToString()
+    {
+        return $"Name is {Name}";
+    }
+}
+
+public record Employee(string? Name, DateTime? DateOfBirth, double? salary) : Person(Name, DateOfBirth)
+{
+    // 'sealed' kerword will guarantee that this '.ToString()' method cannot be overriden by child record
+    public sealed override string ToString()
+    {
+        return $"Salary is {salary}, {base.ToString()}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new("William", DateTime.Parse("1995-04-06"), 6000);
+        Console.WriteLine(employee);
+
+        Console.ReadKey();
+    }
+}
