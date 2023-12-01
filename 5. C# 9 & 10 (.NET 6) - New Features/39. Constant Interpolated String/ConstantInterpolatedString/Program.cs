9680eb703c26d65004af5b8454b3b961f0085f6c
Yosendra <yosendra.evri@gmail.com>
2023-12-01 21:23:15 +0800

    .NET 6 Features - 39. Constant Interpolated String

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/39. Constant Interpolated String/ConstantInterpolatedString/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Constant string may be initialized using 'string interpolation' i.e. with ${},
+// if all the placeholders are constant strings
+
+class Example
+{
+    public const string BaseUrl = "http://www.example.com";
+    
+    // this is in the past, we repeat the domain
+    public const string ApiUrl_1 = "http://www.example.com/api";
+
+    // this is the new feature with string interpolation
+    // notice the placeholder should be paired with constant string 
+    public const string ApiUrl_2 = $"{BaseUrl}/api";
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Example.BaseUrl);
+        Console.WriteLine(Example.ApiUrl_1);
+        Console.WriteLine(Example.ApiUrl_2);
+
+        Console.ReadKey();
+    }
+}
