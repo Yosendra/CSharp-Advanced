84a4dabd946f0f2bf9961e64b301b3e2205c7e44
Yosendra <yosendra.evri@gmail.com>
2023-12-01 15:41:03 +0800

    .NET 6 Features - 35. Command Line Argument - Part 2

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/35. Command Line Argument - Part 2/App2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// C# still support Command Line Argument in top level statement
+
+// Access 'args' variable immediately
+Console.WriteLine(args.Length);
+
+// Assume first argument is a filePath
+if (args.Length > 0)
+{
+    if (File.Exists(args[0]))
+    {
+        string content = File.ReadAllText(args[0]);
+        Console.WriteLine(content);
+    }
+    else
+    {
+        Console.WriteLine("File not found");
+    }
+}
+else
+{
+    Console.WriteLine("FilePath is expected.");
+}
+Console.Write("\nPress any key to exit...");
+Console.ReadKey();
