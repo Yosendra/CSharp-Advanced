47b01eed2e8b3bb9bf6a821fb0b69aba71cd5267
Yosendra <yosendra.evri@gmail.com>
2023-12-01 21:07:43 +0800

    .NET 6 Features - 38. Lambda Function Return Type

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/38. Lambda Function Return Type/LambdaExpressionExample/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// A lambda expression (or lambda function) can have a return type before the list of parenthesized parameters
+
+class Program
+{
+    static void Main()
+    {
+        var getMessage = () => "Hello World";
+
+        // Scenario 1
+        Func<int, object> getResult = (mark) => 
+        {
+            if (mark >= 35)
+                return "Pass";
+            else
+                return 0;
+        };
+        var tempResult = getResult(10);
+        Console.WriteLine(tempResult.GetType());
+        Console.WriteLine(tempResult);
+
+        // Scenario 2
+        var GetNumbers = IList<int> () => new int[] { 1, 2, 3 };
+        var result = GetNumbers();
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
+    }
+}
