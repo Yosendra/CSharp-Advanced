578c4519b0ce81480722c004465f33f2b67347cc
Yosendra <yosendra.evri@gmail.com>
2023-12-02 17:54:29 +0800

    .NET 6 Features - 44. Index-From-End Operator

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/44. Index-From-End Operator/IndexFromEndExample/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// ^ (carret) => this operator returns the index from end of the array (index starts from 1)
+// ex: array[^1]
+
+List<string> myFriends = new()
+{
+    "Scott", "Allen", "Jones", "James", "Sara"
+};
+
+// Traditional way to get the last index of an element in array
+Console.WriteLine(myFriends[myFriends.Count - 1]);
+
+// Using '^' operator
+Console.WriteLine(myFriends[^1]);
+
+Console.ReadKey();
