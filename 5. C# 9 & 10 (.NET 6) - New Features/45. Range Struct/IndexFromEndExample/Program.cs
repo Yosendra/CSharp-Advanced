9d5e1db10dcae7aefb26158b706e688cdec59c83
Yosendra <yosendra.evri@gmail.com>
2023-12-03 02:08:27 +0800

    .NET 6 Features - 45. Range Struct

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/45. Range Struct/IndexFromEndExample/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Range (..) Opearator
+
+List<string> myFriendsList = new()
+{
+    "Scott", "Allen", "Jones", "James", "Sara"
+};
+
+string[] myFriendsArray = new string[]
+{
+    "Scott", "Allen", "Jones", "James", "Sara"
+};
+
+// Skip "Scott", take next three element
+
+// Range - LINQ
+IEnumerable<string> shortListedElements1 = myFriendsList.Skip(1).Take(3);
+foreach (string friend in shortListedElements1)
+    Console.Write($"{friend}, ");
+Console.WriteLine();
+
+// Range - Range Struct : currently only working on Array, not List
+Range range = 1..4;
+string[] shortListedElements2 = myFriendsArray[range];
+//string[] shortListedElements2 = myFriendsArray[1..4];
+foreach (string friend in shortListedElements2)
+    Console.Write($"{friend}, ");
+
+Console.ReadKey();
