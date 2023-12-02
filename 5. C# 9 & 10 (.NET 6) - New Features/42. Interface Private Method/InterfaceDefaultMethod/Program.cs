d6d1ff8426e90f508e7c4d30fd51acb8b5393de0
Yosendra <yosendra.evri@gmail.com>
2023-12-02 14:23:19 +0800

    .NET 6 Features - 42. Interface Private Method

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/42. Interface Private Method/InterfaceDefaultMethod/Program.cs
@@ -0,0 +1,74 @@
+﻿
+// Intrface method can have any access modifier including private, internal, protected, private protected, public
+
+using InterfaceEnchancement;
+
+namespace InterfaceEnchancement
+{
+    public interface IEmployee
+    {
+        public string Name { get; set; }
+
+        // abstract method
+        internal string GetNameInLowerCase();
+        internal int GetNameLength();
+
+        // For 'private' modifier, it must implement the body method
+        private string GetNameTitle()
+        {
+            return "Mr./Ms.";
+        }
+
+        // Since .GeNameTitle() is private, it only can be called inside the interface, not outside
+        internal string GetNameUpperCase()
+        {
+            return GetNameTitle() + Name.ToUpper();
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public string Name { get; set; }
+
+        public double GetSalary() => 0;
+
+        // Option 1: You can explicitly implement non-public abstract interface method
+
+        // Beside 'public' modifier, we can override the interface method immediately
+        // but now it is other than 'public' ('internal' in this case)
+        // so we need explicitly mention the the interface the method belong ('IEmployee')
+        string IEmployee.GetNameInLowerCase()
+        {
+            return Name.ToLower();
+        }
+
+        // Option 2: You can convert the non-public abstract interface method as public
+        public int GetNameLength()
+        {
+            return Name.Length;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    { 
+        Manager manager = new Manager();
+        manager.Name = "William";
+
+        Console.WriteLine(manager.Name);
+
+        IEmployee iEmp = manager;
+        Console.WriteLine(iEmp.GetNameUpperCase());
+
+        // for the option 1, the invocation only when we use the interface type
+        Console.WriteLine(iEmp.GetNameUpperCase());
+
+        // for the option 2, the invocation is enable either when we use the interface type or class type
+        Console.WriteLine(iEmp.GetNameLength());
+        Console.WriteLine(manager.GetNameLength());
+
+        Console.ReadKey();
+    }
+}
