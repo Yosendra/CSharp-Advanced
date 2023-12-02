bf2acd9229ef77cccfca35bed83dc8a357abbb26
Yosendra <yosendra.evri@gmail.com>
2023-12-02 14:33:18 +0800

    .NET 6 Features - 43. Interface Static Member

---
+++ 5. C# 9 & 10 (.NET 6) - New Features/43. Interface Static Member/InterfaceDefaultMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿
+// Static method are allowed with concrete implement in interface
+
+using InterfaceEnchancement;
+
+namespace InterfaceEnchancement
+{
+    public interface IEmployee
+    {
+        public string Name { get; set; }
+
+        // abstract method
+        internal string GetNameInLowerCase();
+        internal int GetNameLength();
+
+        // static interface method
+        public static string GetNameTitle()
+        {
+            return "Mr./Ms.";
+        }
+
+        internal string GetNameUpperCase()
+        {
+            return GetNameTitle() + Name.ToUpper();
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public string Name { get; set; }
+
+        public double GetSalary() => 0;
+
+        string IEmployee.GetNameInLowerCase()
+        {
+            return Name.ToLower();
+        }
+
+        public int GetNameLength()
+        {
+            return Name.Length;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    { 
+        Manager manager = new Manager();
+        manager.Name = "William";
+
+        Console.WriteLine(manager.Name);
+
+        IEmployee iEmp = manager;
+        Console.WriteLine(iEmp.GetNameUpperCase());
+        Console.WriteLine(iEmp.GetNameUpperCase());
+        Console.WriteLine(iEmp.GetNameLength());
+        Console.WriteLine(manager.GetNameLength());
+
+        // calling the static interface method
+        Console.WriteLine(IEmployee.GetNameTitle());
+
+        Console.ReadKey();
+    }
+}
